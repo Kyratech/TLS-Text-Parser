d6968d3c3e514f5d4d3f08520872581232848b3e
Matthew Langford <Mathew.Langford@softwire.com>
2017-07-05 10:22:02 +0100

    Added method to naively count instances of 'tra' in text

--- TLS_TextParser/TLSParser.cs
+++ TLS_TextParser/TLSParser.cs
@@ -14,7 +14,9 @@ namespace TLS_TextParser
 
         static void Main(string[] args)
         {
-            
+            TLSParser tlsParser = new TLSParser("C:/Work/Training/TLS_TextParser/TLS_TextParser/text/source_file.txt");
+            int traCount = tlsParser.CountTLS_tra();
+            Console.WriteLine("'tra' Count = " + traCount);
         }
 
         public TLSParser(String filePath)
@@ -27,6 +29,27 @@ namespace TLS_TextParser
             {
                 throw new ArgumentException(InvalidFileMessage, ae);
             }
+        }
+
+        public int CountTLS_tra()
+        {
+            int counter = 0;
+
+            string[] lines = textReader.GetLines();
+
+            for(int line = 0; line < lines.Length; line++)
+            {
+                char[] currentLine = lines[line].ToCharArray();
+                for(int character = 0; character < currentLine.Length - 2; character++)
+                {
+                    if (char.ToUpperInvariant(currentLine[character]) == 'T' && char.ToUpperInvariant(currentLine[character + 1]) == 'R' && char.ToUpperInvariant(currentLine[character + 2]) == 'A')
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            return counter;
         }
     }
 }
